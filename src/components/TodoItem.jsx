2055ca2533d1a53ce2698a85a3a1c2ab5f42fcb8
afrinaldiGenzo <afrinaldi190401@gmail.com>
2023-07-21 21:24:44 +0700

    use context

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TodoContext } from "../App";
 
-const TodoItem = ({ todo, toggleCompleted, deleteTodo }) => {
+const TodoItem = ({ todo }) => {
+  const { toggleCompleted, deleteTodo } = useContext(TodoContext);
+
   const getTodoTitleStyle = () => {
     if (todo.completed === true) {
       return { textDecoration: "line-through" };
@@ -11,36 +14,42 @@ const TodoItem = ({ todo, toggleCompleted, deleteTodo }) => {
 
   return (
     <div style={styles.todoItem}>
-      <input type="checkbox" style={styles.checkbox} onChange={() => toggleCompleted(todo.id)}/>
+      <input
+        type="checkbox"
+        style={styles.checkbox}
+        onChange={() => toggleCompleted(todo.id)}
+      />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
-      <button style={styles.button} onClick={() => deleteTodo(todo.id)}>x</button>
+      <button style={styles.button} onClick={() => deleteTodo(todo.id)}>
+        x
+      </button>
     </div>
   );
 };
 
 const styles = {
-    todoItem: {
-        border: '2px solid #f4f4f4',
-        fontSize: '24px',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '0 20px',
-      },
-      checkbox: {
-        height: '18px',
-        width: '18px',
-      },
-      button: {
-        backgroundColor: '#BB0000',
-        color: '#fff',
-        height: '30px',
-        width: '30px',
-        borderRadius: '100%',
-        border: 'none',
-        cursor: 'pointer',
-        fontSize: '16px',
-      },
+  todoItem: {
+    border: "2px solid #f4f4f4",
+    fontSize: "24px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "0 20px",
+  },
+  checkbox: {
+    height: "18px",
+    width: "18px",
+  },
+  button: {
+    backgroundColor: "#BB0000",
+    color: "#fff",
+    height: "30px",
+    width: "30px",
+    borderRadius: "100%",
+    border: "none",
+    cursor: "pointer",
+    fontSize: "16px",
+  },
 };
 
 export default TodoItem;
